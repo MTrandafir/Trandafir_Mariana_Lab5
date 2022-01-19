5425657afbbfa6604c8c99319d227f04e659b1ea
Mariana Trandafir <trandafirmariana08@outlook.com>
2022-01-19 23:25:25 +0200

    fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AutoLotModel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
